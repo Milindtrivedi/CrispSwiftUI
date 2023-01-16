c59e4384975512725a6636d1ca53419c6b56e932
Milind Trivedi <milindtrivedi47@gmail.com>
2023-01-16 17:55:08 +0530

    Details view partially completed

--- Fructus/Fructus/App/FruitDetailsView.swift
+++ Fructus/Fructus/App/FruitDetailsView.swift
@@ -14,12 +14,61 @@ struct FruitDetailsView: View {
     
     //MARK: - BODY
     var body: some View {
-        Text(fruit.title)
+        NavigationView {
+            ScrollView(.vertical, showsIndicators: false) {
+                VStack(alignment: .center, spacing: 20) {
+                    //:HEADER
+                    FruitHeaderView(fruit: fruit)
+                    
+                    
+                    VStack(alignment: .leading, spacing: 20) {
+                        //:TITLE
+                        
+                        Text(fruit.title)
+                            .font(.largeTitle)
+                            .fontWeight(.heavy)
+                            .foregroundColor(fruit.gradientColors[1])
+                        
+                        //:HEADLINE
+                        Text(fruit.headline)
+                            .font(.headline)
+                            .multilineTextAlignment(.leading)
+                        
+                        //:NUTRIENTS
+                        
+                        
+                        //:SUBHEADLINE
+                        Text("Learn more about \(fruit.title)".uppercased())
+                            .fontWeight(.bold)
+                            .foregroundColor(fruit.gradientColors[1])
+                        
+                        
+                        //:DESCRIPTION
+                        Text(fruit.description)
+                            .multilineTextAlignment(.leading)
+                        
+                        //:LINK
+                        SourceLinkView()
+                            .padding(.top,10)
+                            .padding(.bottom,40)
+                        
+                    }//: VSTACK
+                    .padding(.horizontal,20)
+                    .frame(maxWidth: 640,alignment: .center)
+                    
+                    
+                }//: VSTACK
+                .navigationBarTitle(fruit.title,displayMode: .inline)
+                .navigationBarHidden(true)
+            }//: SCROLL
+            .edgesIgnoringSafeArea(.top)
+        }//: NAVIGATION
     }
 }
 //MARK: - PREVIEW
 struct FruitDetailsView_Previews: PreviewProvider {
     static var previews: some View {
         FruitDetailsView(fruit: fruitsData[0])
+            .previewDevice("iPhone 14 Pro Max")
     }
 }
---
+++ Fructus/Fructus/Views/SourceLinkView.swift
@@ -0,0 +1,32 @@
+//
+//  SourceLinkView.swift
+//  Fructus
+//
+//  Created by Apple on 16/01/23.
+//
+
+import SwiftUI
+
+struct SourceLinkView: View {
+    var body: some View {
+        
+        GroupBox(){
+            HStack{
+                Text("Content Source")
+                Spacer()
+                Link("Wikipedia", destination: URL(string:"https://wikipedia.com")!)
+                Image(systemName: "arrow.up.right.square")
+            }
+            .font(.footnote)
+        }
+        
+    }
+}
+
+struct SourceLinkView_Previews: PreviewProvider {
+    static var previews: some View {
+        SourceLinkView()
+            .previewLayout(.sizeThatFits)
+            .padding()
+    }
+}
